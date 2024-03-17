b53d7a454711578fc89f947ae3a8416a7c70c172
Travis Craft <tlcraft@users.noreply.github.com>
2024-03-17 10:53:25 -0600

    Edit quotes.

--- src/date-parser/date-parser.ts
+++ src/date-parser/date-parser.ts
@@ -1,20 +1,19 @@
-import moment from "moment-timezone";
-import { BaseHandler } from "../common/handler";
-import { BaseArguments } from "../common/interfaces/base-arguments";
+import moment from 'moment-timezone';
+import { BaseHandler } from '../common/handler';
+import { BaseArguments } from '../common/interfaces/base-arguments';
 
 export class DateParser extends BaseHandler<BaseArguments> {
     run(): void {
-        console.log("Date Parser");
+        console.log('Date Parser');
         const momentCurrentDate = moment(new Date());
-        console.log("momentCurrentDate", momentCurrentDate);
+        console.log('momentCurrentDate', momentCurrentDate);
 
-        const eastern = moment.tz(momentCurrentDate, "America/New_York");
-        console.log("eastern", eastern);
+        const eastern = moment.tz(momentCurrentDate, 'America/New_York');
+        console.log('eastern', eastern);
 
         const dateString = '03/16/2024, 00:00:00';
-        const momentFromDateString = moment(dateString, "MM/DD/YYYY, HH:mm:ss").tz("America/New_York");
-        console.log("momentFromDateString", momentFromDateString);
-                
+        const momentFromDateString = moment(dateString, 'MM/DD/YYYY, HH:mm:ss').tz('America/New_York');
+        console.log('momentFromDateString', momentFromDateString); 
     }
 }
 
